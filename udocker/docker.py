kyoushuu/udocker

commit c11fc19d561e210ab3ce655b01e5eefbf0a268b1

fix _parse_imagerepo()

udocker/docker.py
@@ -491,7 +491,6 @@ class DockerIoAPI(object):
 
     def _parse_imagerepo(self, imagerepo):
         """Parse imagerepo to extract registry"""
-        remoterepo = imagerepo
         registry = ""
         registry_url = ""
         index_url = ""
@@ -499,7 +498,7 @@ class DockerIoAPI(object):
         if '.' in components[0] and len(components) >= 2:
             registry = components[0]
             del components[0]
-        elif ('.' not in components[0] and
+        if ('.' not in components[0] and
               components[0] != "library" and len(components) == 1):
             components.insert(0, "library")
         remoterepo = '/'.join(components)

commit bf9650497138c0de5bb6e29b1b9346dca6758d7c

Fix regression introduced in commit c11fc19

udocker/docker.py
@@ -514,7 +514,7 @@ class DockerIoAPI(object):
         if '.' in components[0] and len(components) >= 2:
             registry = components[0]
             del components[0]
-        if ('.' not in components[0] and
+        elif ('.' not in components[0] and
                 components[0] != "library" and len(components) == 1):
             components.insert(0, "library")
         remoterepo = '/'.join(components)